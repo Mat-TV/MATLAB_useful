cd32256317875b009dfd52ed0b7cb7d15696400b
Mat Troncoso Villar <82126178+Trufumut@users.noreply.github.com>
2021-08-13 17:17:40 -0400

    Ahora sí funciona el comando clear

--- aseo.m
+++ aseo.m
@@ -1,4 +1,4 @@
-%Función para limpiar el entorno de trabajo
+%FunciÃ³n para limpiar el entorno de trabajo
  % a pedido de @Fetiki
 %Argumentos de entrada:
     %nada: nada
@@ -6,7 +6,7 @@
     %S: nada
 %  2021-08-13 - Mat Troncoso Villar (matro1432@gmail.com)
 function S = aseo(nada)
-  clear;
   close;
+  evalin('base','clear');
   clc;
 end
