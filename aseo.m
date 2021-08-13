5e153f98777d7461ca900672c1e20484c3ad21b8
Mat Troncoso Villar <82126178+Trufumut@users.noreply.github.com>
2021-08-13 16:59:53 -0400

    Limpia el entorno de trabajo

---
+++ aseo.m
@@ -0,0 +1,12 @@
+%Función para limpiar el entorno de trabajo
+ % a pedido de @Fetiki
+%Argumentos de entrada:
+    %nada: nada
+%Argumentos de salida:
+    %S: nada
+%  2021-08-13 - Mat Troncoso Villar (matro1432@gmail.com)
+function S = aseo(nada)
+  clear;
+  close;
+  clc;
+end
